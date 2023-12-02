945fa1d0e7afeb5b386b68de83d17f5ce1eb601d
KRISHA-SALIYA <149802459+KRISHA-SALIYA@users.noreply.github.com>
2023-12-02 18:50:19 +0530

    Add files via upload

---
+++ pr-2/KS-PR2-2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a;
+	clrscr();
+
+	printf("enter any number  : ");
+	scanf("%d",&a);
+
+	if(a%2=1)
+	{
+		printf("enetr odd number : ");
+	}
+	else
+	{
+		printf("enter even number : ");
+	}
+	getch();
+}
