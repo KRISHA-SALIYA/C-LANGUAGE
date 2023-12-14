b2662dc9ae822fef1d44ce2e111d546e2c2efe27
Krisha Saliya <149802459+KRISHA-SALIYA@users.noreply.github.com>
2023-12-14 19:42:13 +0530

    Add files via upload

---
+++ ch-9-2/ks-9-2-1.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char psw[10];
+
+  int len,i;
+
+  int upr = 0, lwr = 0, digit = 0, specialsymbol = 0;
+
+    printf("Create your password: ");
+    scanf("%s", &psw);
+
+  for (i = 0; psw[i] != '\0'; i++)
+  {
+    len++;
+  }
+  for(i=0;i<len;i++)
+  {
+
+    if (upr == 1 && lwr == 1 && digit == 0 && specialsymbol == 1)
+    {
+    	len++;
+    }
+  }
+  for ( i = 0; i < len; i++)
+  {
+    if (psw[i] >= 65 && psw[i] <= 90)
+    {
+      upr == 1;
+    }
+    else if (psw[i] >= 97 && psw[i] <= 122)
+    {
+      lwr == 1;
+    }
+    else if (psw[i] >= 48 && psw[i] <= 57)
+    {
+      digit == 1;
+    }
+    else if (psw[i] >= 33 && psw[i] <= 47)
+    {
+      specialsymbol == 1;
+    }
+    
+  }  
+   
+      if (len > 6)
+      {
+        puts("your password is strong ");
+      }
+      else
+      {
+        puts("your password is not strong");
+      }
+<<<<<<< HEAD
+    }
+=======
+}
+>>>>>>> 9834f8355593ffcbad8dad68c27ca34642e4dd1b#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char psw[10];
+
+  int len,i;
+
+  int upr = 0, lwr = 0, digit = 0, specialsymbol = 0;
+
+    printf("Create your password: ");
+    scanf("%s", &psw);
+
+  for (i = 0; psw[i] != '\0'; i++)
+  {
+    len++;
+  }
+  for(i=0;i<len;i++)
+  {
+
+    if (upr == 1 && lwr == 1 && digit == 0 && specialsymbol == 1)
+    {
+    	len++;
+    }
+  }
+  for ( i = 0; i < len; i++)
+  {
+    if (psw[i] >= 65 && psw[i] <= 90)
+    {
+      upr == 1;
+    }
+    else if (psw[i] >= 97 && psw[i] <= 122)
+    {
+      lwr == 1;
+    }
+    else if (psw[i] >= 48 && psw[i] <= 57)
+    {
+      digit == 1;
+    }
+    else if (psw[i] >= 33 && psw[i] <= 47)
+    {
+      specialsymbol == 1;
+    }
+    
+  }  
+   
+      if (len > 6)
+      {
+        puts("your password is strong ");
+      }
+      else
+      {
+        puts("your password is not strong");
+      }
+
+    }
+}
+
